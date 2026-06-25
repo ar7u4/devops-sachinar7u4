a956cc8b676ef94d241b9be7cb080c68ee9c0df6
Arjun <arjun.betageri@live.com>
2026-06-25 21:55:03 +0530

    adding my first code

---
+++ main.py
@@ -0,0 +1,8 @@
+number1 = 5
+number2 = 10
+
+result = number1 + number2
+result2 = number1 * number2
+
+print("The sum of", number1, "and", number2, "is:", result)
+print("The product of", number1, "and", number2, "is:", result2)
